274ec8895108400d411e5c2b76eb421156204a21
andrew-lopez-27 <anl087@ucsd.edu>
2026-04-27 17:41:41 -0700

    fixed bugs in the scripts file

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -10,7 +10,7 @@ function sumValues(num1, num2, add) {
         if (typeof num1 !== 'number' || typeof num2 !== 'number') {
             return false;
         }
-
+        
         const result = num1 + num2;
 
         return result;
